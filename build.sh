c213f7de0945ac31fc65f6d76fdac5f5fd4ef6f5
Ashwani Singh <ashwani.singh@opstree.com>
2023-12-20 23:04:44 +0530

    add azure cli permission

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 source /opt/buildpiper/shell-functions/aws-functions.sh
+source /opt/buildpiper/shell-functions/azure-functions.sh
 
 logInfoMessage "Creating for $MODULE"
 tfCodeLocation="${WORKSPACE}"/"${CODEBASE_DIR}"/"${TF_CODE_LOCATION}"
@@ -15,6 +16,8 @@ cd  "${tfCodeLocation}"
 
 logInfoMessage "Running below tf command"
 logInfoMessage "terraform $INSTRUCTION"
+
+getAzureServicePrinciple $ARM_CLIENT_ID $ARM_CLIENT_SECRET $ARM_TENANT_ID
 
 terraform init
 
