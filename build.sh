56c2c80dd81aa9d65803411f0444dfb9777f8141
Aayush Verma <aayush.verma@opstree.com>
2025-12-04 22:04:52 +0530

    Refactor build script for improved logging and role handling

--- build.sh
+++ build.sh
@@ -1,26 +1,41 @@
 #!/bin/bash
+
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
-source /opt/buildpiper/shell-functions/aws-functions.sh
 
-logInfoMessage "Creating for $MODULE"
+
 tfCodeLocation="${WORKSPACE}"/"${CODEBASE_DIR}"/"${TF_CODE_LOCATION}"
-logInfoMessage "I'll create/update [$MODULE] available at [$tfCodeLocation]"
-sleep  "$SLEEP_DURATION"
-
-getAssumeRole ${AWS_ASSUME_ROLE_ARN}
+logInfoMessage "I'll create/update terraform code available at [$tfCodeLocation]"
 
 cd  "${tfCodeLocation}"
-#cp /opt/buildpiper/modules/* .
+
 
 logInfoMessage "Running below tf command"
 logInfoMessage "terraform $INSTRUCTION"
+if [ "$ASSUME_OTHER_ROLE" == true ]
+then
+	role_output=$(aws sts assume-role --role-arn arn:aws:iam::$ACCOUNT_ID:role/$ROLE_NAME --role-session-name $ROLE_SESSION_NAME)
+
+	if [ $? -ne 0 ]; then
+	  echo "Failed to assume role."
+	  exit 1
+	fi
+
+	AWS_ACCESS_KEY_ID=$(echo $role_output | jq -r '.Credentials.AccessKeyId')
+	AWS_SECRET_ACCESS_KEY=$(echo $role_output | jq -r '.Credentials.SecretAccessKey')
+	AWS_SESSION_TOKEN=$(echo $role_output | jq -r '.Credentials.SessionToken')
+
+	# Export the variables
+	export AWS_ACCESS_KEY_ID
+	export AWS_SECRET_ACCESS_KEY
+	export AWS_SESSION_TOKEN
+fi
 
 terraform init
-
 case "$INSTRUCTION" in
 
   plan)
+    terraform init
     terraform plan -var-file="terraform.tfvars"
     ;;
 
@@ -33,6 +48,6 @@ case "$INSTRUCTION" in
     ;;
 
   *)
-    logInfoMessage "Not a valid option"
+    logInfoMessage "Not a valid option use(plan|apply|destroy)"
     ;;
 esac
